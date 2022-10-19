fc21a0b13c157aad62a40171fcd13fc06285a0a2
romantrety <114093642+22057221@users.noreply.github.com>
2022-10-19 18:31:31 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    //Series1◦
+        //.Даны десять вещественных чисел.Найти их сумму.
+    //int  war = 0, a;  
+    //int i;
+    //for (i = 1; i <= 10; ++i) {
+        //cout << i << "  ";
+        //cin >> a;
+        //war += a;
+    //}
+   //cout << war;
+//}
+
+
+
+
+
+
+//Series2.Даны десять вещественных чисел.Найти их произведение.
+   // int war = 1, a;
+   // int i;
+    //while (i >= 10)++i;
+        //cout << i << "  ";
+        //cin >> a;
+       // war *= a;
+    //cout << war;
+//}
+
+
+
+
+
+    //Series3.Даны десять вещественных чисел.Найти их среднее арифметическое.
+   // float  war = 1, a;
+   // int i;
+   // for (i = 1; i <= 10; ++i) {
+    //    cout << "i" << i << "- ";
+     //   cin >> a;
+    //    war += a;
+   // }
+    //war = war / 10;
+    //cout << war;
+//}
+
+
+
+
+
+
+   // Series4.Дано целое число N и набор из N вещественных чисел.Вывести
+      //  сумму и произведение чисел из данного набора.
+   // float  sum = 0, proiz = 1, a;
+   // int i;
+   // for (i = 1; i <= 10; ++i) {
+   //     cout << i << "  ";
+   // /    cin >> a;
+    //    sum += a;
+    //    proiz *= a;
+   // }
+   // cout << "sum" << sum << endl << "proiz" << proiz;
+//}
